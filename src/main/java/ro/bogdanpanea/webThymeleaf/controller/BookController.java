b9a08384be131e695bd711abcfc6d887dd7414cc
BogdanPanea <tombird83@yahoo.com>
2022-02-10 22:59:15 +0200

    MVC controller

---
+++ src/main/java/ro/bogdanpanea/webThymeleaf/controller/BookController.java
@@ -0,0 +1,30 @@
+package ro.bogdanpanea.webThymeleaf.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import ro.bogdanpanea.webThymeleaf.pojo.Book;
+
+@Controller
+public class BookController {
+
+    @Value("${greeting}")
+    private String greeting;
+
+    @GetMapping("/books")
+    public String getAllBooks(Model model) {
+        //Returns hardcoded data, a real world application would return from the database
+        List<Book> books = new ArrayList<Book>();
+        books.add(new Book(1, "Emma", "Jane Austen"));
+        books.add(new Book(2, "Harry Potter", "JK Rowling"));
+        books.add(new Book(3, "The Partner", "John Grisham"));
+        model.addAttribute("books", books);
+        model.addAttribute("greeting", greeting);
+        return "books";
+    }
+
+}
