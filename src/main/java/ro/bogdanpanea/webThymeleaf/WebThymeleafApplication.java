c22b052a00bb36578cc7d2970cd63a512afc9100
BogdanPanea <tombird83@yahoo.com>
2022-02-11 00:08:01 +0200

    war file

--- src/main/java/ro/bogdanpanea/webThymeleaf/WebThymeleafApplication.java
+++ src/main/java/ro/bogdanpanea/webThymeleaf/WebThymeleafApplication.java
@@ -2,12 +2,19 @@ package ro.bogdanpanea.webThymeleaf;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class WebThymeleafApplication {
+public class WebThymeleafApplication extends SpringBootServletInitializer {
 
-	public static void main(String[] args) {
-		SpringApplication.run(WebThymeleafApplication.class, args);
-	}
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(WebThymeleafApplication.class);
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebThymeleafApplication.class, args);
+    }
 
 }
